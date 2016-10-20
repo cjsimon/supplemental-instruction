f3300406a51fee66b6b23442b0799d581f53130d
Christopher Simon <cjsimon333@gmail.com>
2016-10-19 22:38:29 -0700

    Added week 6

--- spr2016/Week 02 - Advanced For Loops/OddDashes.java
+++ spr2016/Week 02 - Advanced For Loops/OddDashes.java
@@ -1,10 +1,10 @@
 public class OddDashes {
 	
-	public static final int SIZE = 5;
+	public static final int SIZE = 3;
 	
 	public static void main(String[] args) {
 		// From lines SIZE through 1, subtracting by two
-		for(int line = SIZE; line >= 1; line -= 2)
+		for(int line = SIZE; line >= 1; line--)
 		{
 			// Print out each dash, from 1 to line
 			for(int dash = 1; dash <= line; dash++)
